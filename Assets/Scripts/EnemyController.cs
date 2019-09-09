6a92b51d8cb54e7cc6a4cb4f2ae20dad5de1ffa2
eldroan <leandroamarillo95@gmail.com>
2019-09-08 22:53:40 -0300

    Se agregan eventos al EnemyController para cuando muere o cuando choca con el castillo

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    [SerializeField] private float maxLife;
+    private float _currentLife;
     // Start is called before the first frame update
+    
+    public static Action<float> HitCastle = delegate(float f) {  };
+    public static Action EnemyDied = delegate { };
 
     // Update is called once per frame
+
+    private void Awake()
+    {
+        _currentLife = maxLife;
+    }
+
     void Update()
     {
         this.transform.Translate(Time.deltaTime * speed * Vector3.left);
@@ -18,7 +30,20 @@ public class EnemyController : MonoBehaviour
     {
         if (other.CompareTag("Castle"))
         {
-            Debug.Log("Auch, estaba re duro ese castillo");   
+            Debug.Log("Auch, estaba re duro ese castillo");
+            HitCastle(damage);
+            
+            //Me destruyo (aunque me deberia poolear para mejorar performance)
+            Destroy(this.gameObject);
+        }
+
+        if (other.CompareTag("Bullet"))
+        {
+            var inflictedDamage = other.gameObject.GetComponent<Bullet>().damage;
+            _currentLife -= inflictedDamage;
+
+            if (_currentLife <= 0)
+                EnemyDied();
         }
     }
 }
