cccf0c23af6c7107d7701c20a88bba082463be43
eldroan <leandroamarillo95@gmail.com>
2019-09-08 22:05:24 -0300

    Se spawnean enemigos que van de izquierda a derecha y chocan con el castillo

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Time.deltaTime * speed * Vector3.left);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Castle"))
+        {
+            Debug.Log("Auch, estaba re duro ese castillo");   
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SpawnerController : MonoBehaviour
+{
+    [SerializeField] private int maxEnemies;
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnRate;
+    private float _timeToNextSpawn;
+    private Vector3 _spawnPosition;
+    private int _currentEnemies;
+
+    private void Awake()
+    {
+        //Assert.IsNotNull(enemyPrefab);
+        _spawnPosition = this.transform.position;
+        _timeToNextSpawn = spawnRate;
+    }
+
+    void Update()
+    {
+        if (_timeToNextSpawn < 0 && _currentEnemies < maxEnemies)
+        {
+            _timeToNextSpawn = spawnRate;
+            var newEnemy = Instantiate(enemyPrefab, _spawnPosition, Quaternion.identity,this.transform);
+            _currentEnemies++;
+        }
+        else
+        {
+            _timeToNextSpawn -= Time.deltaTime;
+        }
+    }
+}
