bdd941363298657fd51fffb9e39463792e0025ac
eldroan <leandroamarillo95@gmail.com>
2019-09-08 22:56:10 -0300

    Se agrega GUI para mostrar la barra de vida que se actualiza cuando un enemigo choca con el castillo

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private float startingCastleLife;
+    private float _currentLife;
+    [SerializeField] private int gems;
+
+    [SerializeField] private GameObject healthBarGameObject;
+
+        // Start is called before the first frame update
+    void Awake()
+    {
+        Assert.AreNotEqual(0f,startingCastleLife,"La vida no puede ser 0");
+        _currentLife = startingCastleLife;
+        EnemyController.HitCastle += HandleHit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HandleHit(float damage)
+    {
+        _currentLife -= damage;
+        healthBarGameObject.transform.localScale = new Vector3(_currentLife/startingCastleLife,1,1);
+    }
+}
