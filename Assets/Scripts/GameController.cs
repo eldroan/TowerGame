8a52632d19b7af34a2f595bc68bbcd8bfbe6328e
eldroan <leandroamarillo95@gmail.com>
2019-09-09 23:45:09 -0300

    Se agregan raycast para saber donde toca el player, cuando el player toca un lugar se pone un indicador

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -10,24 +11,50 @@ public class GameController : MonoBehaviour
     [SerializeField] private int gems;
 
     [SerializeField] private GameObject healthBarGameObject;
+    [SerializeField] private GameObject selectedSpotWaypoint;
+    private Camera _mainCamera;
+    private Transform _selectedSpot;
 
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Awake()
     {
-        Assert.AreNotEqual(0f,startingCastleLife,"La vida no puede ser 0");
+        Assert.AreNotEqual(0f, startingCastleLife, "La vida no puede ser 0");
         _currentLife = startingCastleLife;
         EnemyController.HitCastle += HandleHit;
+        _mainCamera = Camera.main;
+        _selectedSpot = null;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            LayerMask layerMask = LayerMask.GetMask("TurretSpot");
+            // Does the ray intersect any objects excluding the player layer
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, layerMask))
+            {
+//                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log($"Did Hit{hit.transform.gameObject.name}");
+                _selectedSpot = hit.transform;
+                
+                //Pongo el waypoint
+                selectedSpotWaypoint.SetActive(true);
+                selectedSpotWaypoint.transform.parent = _selectedSpot;
+                selectedSpotWaypoint.transform.localPosition = Vector3.zero;
+            }
+            else
+            {
+                selectedSpotWaypoint.SetActive(false);
+                _selectedSpot = null;
+            }
+        }
     }
 
     void HandleHit(float damage)
     {
         _currentLife -= damage;
-        healthBarGameObject.transform.localScale = new Vector3(_currentLife/startingCastleLife,1,1);
+        healthBarGameObject.transform.localScale = new Vector3(_currentLife / startingCastleLife, 1, 1);
     }
 }
