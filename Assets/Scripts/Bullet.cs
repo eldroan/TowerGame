e73543b92a916cf059122f9745afa77182729320
eldroan <leandroamarillo95@gmail.com>
2019-09-12 22:57:38 -0300

    Script de balas que lo unico que hacen es ir hacia los enemigos y chocarlos

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 public class Bullet : MonoBehaviour
-{ 
-    public float damage;
+{
+    [SerializeField] private float speed;
+    private Transform _target = null;
+    private bool _readyToGo = false;
+    public float Damage { get; private set; }
+
+    private void FixedUpdate()
+    {
+        if (_readyToGo)
+        {
+            this.transform.LookAt(_target);
+            this.transform.Translate(Vector3.forward * speed);
+        }
+    }
+
+    public void Initialize(float damage, Transform target)
+    {
+        Damage = damage;
+        _target = target;
+        _readyToGo = true;
+    }
 }
