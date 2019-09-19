eec506c1b846122d498dfba63db9ad74b2d7c489
Bruno Andres <brunoandres89@outlook.com>
2019-09-18 21:50:17 -0300

    Todo para el taller de animación

---
+++ Assets/Taller animation/Scripts/CinematicTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CinematicTrigger : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector director;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        director.Play();
+    }
+}
--- Assets/Taller animation/Scripts/Follow.cs
+++ Assets/Taller animation/Scripts/Follow.cs
@@ -15,6 +15,6 @@ public class Follow : MonoBehaviour
     void Update()
     {
         if(target != null)
-            this.transform.position = target.transform.position + new Vector3(-1f, 1.3f, -1.3f);
+            this.transform.position = target.transform.position + new Vector3(-2.1f, 1.3f, -1.3f);
     }
 }
